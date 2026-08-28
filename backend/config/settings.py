55ad8842eb06c19cba80e58ed576d770f2422f2c
Boburjon <boburjonabduganiyev83@gmail.com>
2026-08-28 13:52:39 +0500

    Set STATIC_ROOT to BASE_DIR / static to match docker volume

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,7 +49,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 LANGUAGE_CODE = "uz"; TIME_ZONE = "Asia/Tashkent"; USE_I18N = True; USE_TZ = True
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
